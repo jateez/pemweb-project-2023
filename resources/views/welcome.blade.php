ba90004d005dd89218687d4867a9fd23f5f14902
Arya Jati <aryajati29@gmail.com>
2023-05-15 17:52:02 +0700

    first-commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to the Group App</title>
+    
+</head>
+<body>
+    <h1>Welcome to the Group App</h1>
+    <a href="{{ route('groups.index') }}">View Groups</a>
+    <br>
+    <a href="{{ route('groups.create') }}">Create a Group</a>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\http\Controllers\GroupController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('/groups', [GroupController::class, 'index'])->name('groups.index');
+Route::get('/groups/create', [GroupController::class, 'create'])->name('groups.create');
+Route::post('/groups', [GroupController::class, 'store'])->name('groups.store');
+Route::delete('/groups/{group}', [GroupController::class, 'delete'])->name('groups.delete');
+Route::get('/groups/{group}', [GroupController::class, 'show'])->name('groups.show');
+
